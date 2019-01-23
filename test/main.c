37f3c15fa603f5fccf6b0d4bffd72a95194324ff
Henning Matyschok <henning.matyschok@stud.fh-flensburg.de>
2019-01-23 19:17:01 +0100

    Example class implemented.

--- test/main.c
+++ test/main.c
@@ -1,3 +1,29 @@
+/*-
+ * Copyright (c) 2019 Henning Matyschok
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
 #include <sys/types.h>
 
 #include <stdlib.h>
@@ -43,6 +69,18 @@ static cobj_method_t foo_methods[] = {
 
 DEFINE_CLASS(foo, foo_methods, sizeof(struct cobj));
 
+/*
+ * Import tailq_class(3).
+ */
+#include "tailq_if.h" 
+ 
+DECLARE_CLASS(tailq_class);
+
+int item_a = 1;
+int item_b = 2;
+int item_c = 3;
+int *tmp;
+
 int
 main(int argc, char *argv[])
 {
@@ -52,7 +90,7 @@ main(int argc, char *argv[])
 	 * Initialize foo_class(3) and call its 
 	 * statically defined method, if any.
 	 */
-	cobj_class_compile(&foo_class);
+	(void)cobj_class_compile(&foo_class);
 	FOO_STATIC_BAR(&foo_class);
 	
 	/* 
@@ -67,14 +105,45 @@ main(int argc, char *argv[])
 	 * Change its type and call the specific
 	 * implementation of foo_bar(3) method.
 	 */
-	cobj_class_free(&null_class);
-	cobj_init(o, &foo_class);
+	(void)cobj_class_free(&null_class);
+	(void)cobj_init(o, &foo_class);
 	FOO_BAR(o);
 	
 	/* 
 	 * Try to call a not implemented method. 
 	 */
 	FOO_BAZ(o, 1);
+	(void)cobj_delete(o);
+	
+	/*
+	 * Initialize tailq_class(3), because 
+	 * tailq_{create,destroy}(3) are static 
+	 * class methods.
+	 */
+	(void)cobj_class_compile(&tailq_class);
+	
+	/*
+	 * Create an instance of tailq_class(3).
+	 */
+	o = TAILQ_CREATE(&tailq_class);
+	
+	/*
+	 * Enqueue.
+	 */
+	(void)TAILQ_ADD(o, &item_a);
+	(void)TAILQ_ADD(o, &item_b);
+	(void)TAILQ_ADD(o, &item_c);
+	
+	/*
+	 * Dequeue.
+	 */
+	while ((tmp = (int *)TAILQ_POLL(o)) != NULL)
+		(void)printf("%s: item: %d\n", __func__, *tmp);
+	
+	/*
+	 * Destroy instance of tailq_class(3).
+	 */
+	(void)TAILQ_DESTROY(&tailq_class, o);
 	
 	exit(EX_OK);
 }
